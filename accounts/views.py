660de1e8341025901be9bedf1541c4333282adcb
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-01-31 15:24:13 -0600

    fixing some spell issues

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,8 @@ from django.views.generic import CreateView
 
 from .forms import CustomUserCreationForm
 
-class SingUpView(CreateView):
+
+class SignUpView(CreateView):
     form_class = CustomUserCreationForm
-    success_url = reverse_lazy("login")
+    success_url = reverse_lazy('login')
     template_name = "registration/signup.html"
