b2dd53596d151369121aae840cbed15df8ab2048
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-02-10 07:56:43 -0600

    added some test and email field

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
+
+
+class SingupPageTests(TestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/accounts/signup/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_view_name(self):
+        response = self.client.get(reverse("singup"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "registration/signup.html")
 
 # Create your tests here.
